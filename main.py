50dfcb96079fb502b8e5ae77b495d779a08b2eaa
dom silva <dmzksh@gmail.com>
2020-07-27 22:03:06 +0400

    add function code to main

--- main.py
+++ main.py
@@ -8,45 +8,36 @@ from model import DQNSolver
 
 register(
     id='cpong-v0',
-    entry_point='env.pong_env:PongEnv',
-    max_episode_steps=10000,
+    entry_point='env.pong_env:PongEnv'
 )
 
 
-def train_pong():
+if __name__ == '__main__':
+
     env = gym.make('cpong-v0')
     observation_space = env.observation_space.shape[0]
     action_space = env.action_space.n
     dqn_solver = DQNSolver(observation_space, action_space)
+    run = 0
     while True:
+        run += 1
         state = env.reset()
         state = np.reshape(state, [1, observation_space])
+        step = 0
         while True:
+            step += 1
             env.render()
             action = dqn_solver.act(state)
             state_next, reward, terminal, info = env.step(action)
             reward = reward if not terminal else -reward
             state_next = np.reshape(state_next, [1, observation_space])
             dqn_solver.remember(state, action, reward, state_next, terminal)
-            dqn_solver.experience_replay()
             state = state_next
             if terminal:
+                print('Run: ' + str(run) + ', exploration: ' + str(dqn_solver.exploration_rate))
                 break
+            dqn_solver.experience_replay()
 
     env.close()
 
-
-if __name__ == '__main__':
-
-    train_pong()
-
-    '''
-    env = gym.make('cpong-v0')
-    env.reset()
-    for _ in range(10000):
-        env.render()
-        env.step(env.action_space.sample()) # take a random action
-        time.sleep(0.04)
-    env.close()
-    '''
     
